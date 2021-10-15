1681ee0fce39053a4f6caba047cc0e14dce689ce
Benson Jiang <benson.jiang@sjsu.edu>
2021-10-14 21:36:13 -0700

    added documentation to timing.py

--- timing.py
+++ timing.py
@@ -1,5 +1,23 @@
 import time;
 def calculate_time(func):
+	'''
+	prints the times it takes to run a function
+
+	Takes note of time then runs specified function. After the function is complete,
+	it will again take note of the time then print out the difference of the two times
+	to give you the time it takes to run the function
+
+	Parameters
+	----------
+	N/A
+
+	Yields
+	------
+	Float
+		The difference between the start time and ending time
+	String
+		Context for the Float
+	'''
 	def inner():
 		x = time.time()
 		z = func()
